b6e1190060c7c9959315fa53c8182418c96fc759
donch <donch@LAPTOP-3CTCCR2M>
2018-04-17 15:31:42 +0900

    Web3年生　Ver1.0 ユーザ名簿をListでメモリ上に保存 UserBeanをSessionScopedに修正

--- src/java/beans/UserBean.java
+++ src/java/beans/UserBean.java
@@ -5,24 +5,37 @@
  */
 package beans;
 
+import data.User;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
 
 /**
  *
  * @author donch
  */
 @Named(value = "userBean")
-@RequestScoped
-public class UserBean {
+@SessionScoped
+public class UserBean implements Serializable {
     private String userId;
     private String userPass;
+    private List<User> users;
     /**
      * Creates a new instance of UserBean
      */
     public UserBean() {
+        users = new ArrayList<>();
     }
 
+    public List<User> getUsers() {
+        return users;
+    }
+
+    
+    
     public String getUserId() {
         return userId;
     }
@@ -42,5 +55,18 @@ public class UserBean {
     public String loginNext(){
         String nextPage="user";
         return nextPage;
+    }
+    
+    public String userAdd(){
+        String nextPage="index";
+        User wUser = new  User();
+        wUser.setUserId(userId);
+        wUser.setUserPassword(userPass);
+        users.add(wUser);
+        return nextPage;
+    }
+    
+    public List<User> userAll(){
+        return users;
     }
 }
